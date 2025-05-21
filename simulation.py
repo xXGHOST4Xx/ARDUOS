dd5ea182ff36213e4969dd33cb2d7a4b903483f4
xXGHOST4Xx <wpa1408.49@gmail.com>
2025-05-21 18:47:28 +0100

    Create simulation.py

---
+++ simulation.py
@@ -0,0 +1,16 @@
+// Simulate Version 0.7 Of ARDUOS
+while True:
+    print("Which option?")
+    choice = input()
+
+    if choice == "1":
+        print("Running 10X LED flash")
+    elif choice == "2":
+        print("2")
+    elif choice == "3":
+        print("3")
+    elif choice == "4":
+        print("4")
+    else:
+        print("Invalid input")
+
