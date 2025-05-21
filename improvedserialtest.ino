7125fa88a208c539998f83eab8573aadaaad61d9
xXGHOST4Xx <wpa1408.49@gmail.com>
2025-05-21 17:17:22 +0100

    0.7
    Initial Public Release

---
+++ improvedserialtest.ino
@@ -0,0 +1,56 @@
+
+int file1 = 0;  
+int file1state = 0;
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  
+  Serial.println("Which option?");
+  
+  while (Serial.available() == 0) {
+
+  }
+ 
+  int menuChoice = Serial.parseInt();  
+
+//  Serial.print("DEBUG: You entered: ");
+//  Serial.println(menuChoice);
+
+ 
+  switch (menuChoice) {
+    case 1:
+      Serial.println("Running 10X LED flash");
+      
+  
+      for (int i = 0; i < 10; i++) {
+        digitalWrite(LED_BUILTIN, HIGH);  
+        delay(500);  
+        digitalWrite(LED_BUILTIN, LOW);  
+        delay(500);  
+      }
+      break;
+      
+    case 2:
+      Serial.println("2");
+      break;
+
+    case 3:
+      Serial.println("3");
+      break;
+    
+    case 4:
+      Serial.println("4");
+      break;
+
+    //default:
+      //Serial.println("ERROR");
+      //break;
+  }
+  
+  delay(50); 
+}
